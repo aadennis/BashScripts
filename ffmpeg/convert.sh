91d97e1023b4c2deb6e3facfa9d219306af13507
aadennis <dennis.circ@gmail.com>
2023-12-03 01:21:46 +0000

    titles and images catting ok

--- ffmpeg/convert.sh
+++ ffmpeg/convert.sh
@@ -17,11 +17,17 @@ opening_title="$3"
 output_duration="$4"
 
 # Generate opening credits video with the provided title
-#ffmpeg -f lavfi -t "$output_duration" -i anullsrc=channel_layout=stereo:sample_rate=44100 -vf "drawtext=text='${opening_title}':fontcolor=white:fontsize=40:x=(w-text_w)/2:y=(h-text_h)/2" -c:v libx264 -r 30 -pix_fmt yuv420p -c:a null -an opening_credits.mp4
-ffmpeg -f lavfi -i color=size=3456x2304:duration=10:rate=25:color=blue -vf "drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:fontsize=300:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='Bowling Green'" 'test_artifacts/opening_credits.mp4'
+#ffmpeg -f lavfi -i color=size=3456x2304:duration=10:rate=25:color=blue -vf "drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:fontsize=300:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='Bowling Green'" 'test_artifacts/opening_credits.mp4'
 
 # Create a video from images
-ffmpeg -framerate 1/3 -pattern_type glob -i "$1" -c:v libx264 -r 30 -pix_fmt yuv420p 'test_artifacts/images.mp4'
+#ffmpeg -framerate 1/3 -pattern_type glob -i "$1" -c:v libx264 -r 30 -pix_fmt yuv420p 'test_artifacts/images.mp4'
+
+# https://ffmpeg.org/faq.html#How-can-I-join-video-files_003f
+#cat 'test_artifacts/opening_credits.mp4' 'test_artifacts/images.mp4' > 'test_artifacts/thelot.mp4'
+#ffmpeg -i 'test_artifacts/thelot.mp4' -qscale:v 2 'test_artifacts/thelotout.mp4'
+#ffmpeg -i 'test_artifacts/opening_credits.mp4' -i 'test_artifacts/images.mp4' 'test_artifacts/outty.mp4'
+ffmpeg -f concat -i filelist.txt -c copy 'test_artifacts/outty.mp4'
+
 
 # Concatenate opening credits and images videos
 #ffmpeg -i opening_credits.mp4 -i images.mp4 -filter_complex "[0:v] [1:v] concat=n=2:v=1 [v]" -map "[v]" -y "$2"
