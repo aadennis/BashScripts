46f76e19fb900c17f3972927741a8d5440f66bb2
aadennis <dennis.circ@gmail.com>
2023-12-03 01:27:15 +0000

    Combined mp4 works

--- ffmpeg/convert.sh
+++ ffmpeg/convert.sh
@@ -17,20 +17,16 @@ opening_title="$3"
 output_duration="$4"
 
 # Generate opening credits video with the provided title
-#ffmpeg -f lavfi -i color=size=3456x2304:duration=10:rate=25:color=blue -vf "drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:fontsize=300:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='Bowling Green'" 'test_artifacts/opening_credits.mp4'
+credits_outfile='test_artifacts/opening_credits.mp4'
+ffmpeg -f lavfi -i color=size=3456x2304:duration=3:rate=25:color=blue -vf "drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:fontsize=300:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='Bowling Green'" $credits_outfile
 
 # Create a video from images
-#ffmpeg -framerate 1/3 -pattern_type glob -i "$1" -c:v libx264 -r 30 -pix_fmt yuv420p 'test_artifacts/images.mp4'
+images_outfile='test_artifacts/images.mp4'
+ffmpeg -framerate 1/3 -pattern_type glob -i "$1" -c:v libx264 -r 30 -pix_fmt yuv420p $images_outfile
 
-# https://ffmpeg.org/faq.html#How-can-I-join-video-files_003f
-#cat 'test_artifacts/opening_credits.mp4' 'test_artifacts/images.mp4' > 'test_artifacts/thelot.mp4'
-#ffmpeg -i 'test_artifacts/thelot.mp4' -qscale:v 2 'test_artifacts/thelotout.mp4'
-#ffmpeg -i 'test_artifacts/opening_credits.mp4' -i 'test_artifacts/images.mp4' 'test_artifacts/outty.mp4'
-ffmpeg -f concat -i filelist.txt -c copy 'test_artifacts/outty.mp4'
+combined_outfile='test_artifacts/outty.mp4'
+ffmpeg -f concat -i filelist.txt -c copy $combined_outfile
 
-
-# Concatenate opening credits and images videos
-#ffmpeg -i opening_credits.mp4 -i images.mp4 -filter_complex "[0:v] [1:v] concat=n=2:v=1 [v]" -map "[v]" -y "$2"
 
 # Clean up temporary files
 #rm opening_credits.mp4 images.mp4
