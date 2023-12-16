33ab77aa14778f129d52cf5a4f9964dcedac70ae
dennis <dennis.circ@gmail.com>
2023-12-16 17:59:29 +0000

    refactor

--- ffmpeg/gist/array_of_filenames.sh
+++ ffmpeg/gist/array_of_filenames.sh
@@ -1,12 +1,13 @@
 #!/bin/zsh
 # How to iterate through an array of filenames
 
-echo "file 1 content" > /Users/den/git/BashScripts/ffmpeg/openai/file1.txt
-echo "file 2 content" > /Users/den/git/BashScripts/ffmpeg/openai/file2.txt
-echo "file 3 content" > /Users/den/git/BashScripts/ffmpeg/openai/file3.txt
-echo "file 4 content" > /Users/den/git/BashScripts/ffmpeg/openai/file4.txt
+working_dir="/Users/den/git/BashScripts/ffmpeg/openai"
+echo "file 1 content" > $working_dir/file1.txt
+echo "file 2 content" > $working_dir/file2.txt
+echo "file 3 content" > $working_dir/file3.txt
+echo "file 4 content" > $working_dir/file4.txt
 
-file_list=($(find /Users/den/git/BashScripts/ffmpeg/openai -iname "*.txt" | sort))
+file_list=($(find $working_dir -iname "*.txt" | sort))
 echo "${file_list[@]}"
 for i in "${file_list[@]}"; do
     if [ -f "$i" ]; then
