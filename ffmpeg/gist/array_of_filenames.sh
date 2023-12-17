e94a1802a1b24ebac4a10ce1dc39cb5c699e89ec
dennis <dennis.circ@gmail.com>
2023-12-17 08:22:18 +0000

    loop to set up test data

--- ffmpeg/gist/array_of_filenames.sh
+++ ffmpeg/gist/array_of_filenames.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 # How to iterate through an array of filenames
 
-working_dir="/Users/den/git/BashScripts/ffmpeg/openai"
-echo "file 1 content" > $working_dir/file1.txt
-echo "file 2 content" > $working_dir/file2.txt
-echo "file 3 content" > $working_dir/file3.txt
-echo "file 4 content" > $working_dir/file4.txt
+workdir="/Users/den/git/BashScripts/ffmpeg/openai"
 
-file_list=($(find $working_dir -iname "*.txt" | sort))
+for i in 1 2 3 4; do
+    echo "file$i content" > $workdir/file$i.txt
+done
+
+file_list=($(find $workdir -iname "*.txt" | sort))
 echo "${file_list[@]}"
 for i in "${file_list[@]}"; do
     if [ -f "$i" ]; then
