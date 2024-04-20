d815b039a6db1610b2fb0f69b8665e6f4cbf5d14
aadennis <dennis.circ@gmail.com>
2024-04-20 13:43:33 +0100

    safe keyword

--- nothing/juxmanx.sh
+++ nothing/juxmanx.sh
@@ -13,7 +13,7 @@ exiftool -d %Y-%m-%d_%s '{}' >> images.txt
 cat ./images.txt
 
 # Generate the video
-ffmpeg -f concat -i images.txt -i watermark.png -filter_complex "
+ffmpeg -safe 0 -f concat -i images.txt -i watermark.png -filter_complex "
 overlay=W-w-10:H-h-10,
 format=yuv420[v];
 [v]drawtext=text='%{localtime\: %T\\n%Y-%m-%d}':
@@ -24,3 +24,6 @@ x=(W-w-10):y=H-h-30:fontsize=14:fontcolor=white:box=1:boxcolor=black@0.5:boxbord
 
 #[concat @ 0x561737a935c0] Unsafe file name '/mnt/d/Sandbox/git/aadennis/BashScripts/nothing/leo_convertxxx.sh'
 #images.txt: Operation not permitted
+# https://stackoverflow.com/questions/38996925/ffmpeg-concat-unsafe-file-name
+# https://www.how2shout.com/linux/how-to-install-and-use-exiftool-on-ubuntu-22-04-or-20-04-lts/
+echo "asdfasdf"
