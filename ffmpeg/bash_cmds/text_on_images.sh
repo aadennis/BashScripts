f5a89ffa704a837b197689e392cce7a07c5bb293
aadennis <dennis.circ@gmail.com>
2024-05-01 18:17:45 +0100

    variable on wildcard - might be broken

--- ffmpeg/bash_cmds/text_on_images.sh
+++ ffmpeg/bash_cmds/text_on_images.sh
@@ -9,16 +9,18 @@
 # single video file in mp4 format.
 
 duration_per_image=3 # seconds
+wild_card="s_*.png"
 # NO EDITING BELOW HERE
 frame_rate=1/$duration_per_image
-
 input_folder="test_artifacts"
 
-# Output folder - this is not created at 
-# runtime, so it must pre-exist the run
+# Output folder - this is not created at runtime, so it must pre-exist the run
 output_folder="test_output" 
+mp4_out_file="woutfile.mp4" # 5.02mb for 1fps, 3.78mb for 30fps sic
 
-for image in "$input_folder"/*.JPG; do
+for image in $input_folder/$wild_card; do
+    echo "Processing [$image]"
+
     # Extract filename with extension
     filename=$(basename -- "$image")
     # (for no extension use...)
@@ -29,19 +31,13 @@ for image in "$input_folder"/*.JPG; do
 
     # Overlay image with filename
     overlay_text="$filename / $creation_date"
-    echo -e "this now $overlay_text !!!!!!!!!!!"
 
     ffmpeg -i "$image" -vf "drawtext=text='$overlay_text':x=50:y=50:fontsize=30:fontcolor=black" "$output_folder/$filename"
     
     echo "Overlay added to $filename"
 done
 
-read -p "\nout of there now"
-
-mp4_out_file="woutfile.mp4" # 5.02mb for 1fps, 3.78mb for 30fps sic
-
 # input is now those jpg etc files with text, output is a single mp4 file
-ffmpeg -framerate $frame_rate -pattern_type glob -i "$output_folder/*.JPG" -c:v libx264 -r 30 -pix_fmt yuv420p $mp4_out_file
+ffmpeg -framerate $frame_rate -pattern_type glob -i $output_folder/$wild_card -c:v libx264 -r 30 -pix_fmt yuv420p $mp4_out_file
 
-read -p "\nSlideshow video is: [$mp4_out_file]\n"
 
