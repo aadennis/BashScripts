a1e5ff20892aa9bf05943e43f48e1852845ad104
aadennis <dennis.circ@gmail.com>
2024-10-09 14:20:07 +0100

    Add variables to m4a splitter

--- ffmpeg/gist/split_m4a.sh
+++ ffmpeg/gist/split_m4a.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
+# Split a sound file of a given type, into n parts of the same type 
+# For example, m4a, mp3, etc.
+# Example usage (bash)
+# ./split_m4a.sh /mnt/c/temp/Lesson_6/Lesson_6.m4a .m4a 4
 
 # Input file
 input_file="$1"
+file_type="$2" # include the dot for now, e.g. .m4a
+number_of_parts=$3
+
 
 # Extract the base name (without extension) from the input file
-base_name=$(basename "$input_file" .m4a)
+base_name=$(basename "$input_file" "$file_type")
 
 # Get the total duration of the file in seconds
 duration=$(ffmpeg -i "$input_file" 2>&1 | grep "Duration" | awk '{print $2}' | tr -d , | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
 
-# Calculate the duration of each part (divide by 4)
-part_duration=$(echo "$duration / 4" | bc)
+# Calculate the duration of each part (divide by $number_of_parts)
+part_duration=$(echo "$duration / $number_of_parts" | bc)
 
 # Loop to create 4 parts
-for i in {0..3}; do
+xx=$((number_of_parts-1))
+echo $xx
+for i in $(seq 0 $xx); do
+  echo "Now we have $i"
   start_time=$(echo "$i * $part_duration" | bc)
   
   # Zero-padded index for the file name
